8f00205eebd9db04d7edb746aa7b72dd471172a0
Robin Miller <robin@tenjin.ca>
2026-06-22 19:48:17 -0600

    Extracted fast login cookie to helper test method

--- faces/web/tests/support/helpers.rb
+++ faces/web/tests/support/helpers.rb
@@ -207,6 +207,39 @@ module HelperMethods
       alias long_press long_click
 
       module Actions
+         # TODO: extract to gem
+         # Generates a login session in a fast way for the current browser.
+         # Driving a direct request via Rack::Test is much faster than driving the web form and waiting for page loads
+         def generate_login_session(path, params)
+            mini_browser = Rack::Test::Session.new(Capybara.app, Capybara.server_host)
+
+            # using cookie_jar because Rack::MockResponse does not parse the http_only cookie flag. Likely a bug.
+            # session_cookie = nil
+            mini_browser.post(path, params.to_json, rack_env) # do |response|
+            #    session_cookie = response.cookies[session_cookie_name]
+            # end
+            session_cookie = mini_browser.cookie_jar.get_cookie session_cookie_name
+
+            raise 'Test Error: No session cookie found' if session_cookie.nil?
+
+            # need to use raw version because the :value in #to_h is not escaped
+            name, value = session_cookie.raw.split('=')
+
+            cookie_hash = session_cookie.to_h.merge(name: name, value: value).transform_keys do |key|
+               case key
+               when String
+                  key.downcase.to_sym
+               else
+                  key
+               end
+            end
+
+            browser = page.driver.browser
+            # Debug note: if this throws a cookie domain error it *might* be because the ssl cert is untrusted by the browser (eg. self-signed)
+            # Debug note: the value here must be the CGI escaped value as appears in the header
+            browser.manage.add_cookie(cookie_hash.except(:path, :domain))
+         end
+
          def api_request(url, params)
             page.driver.post url, params.to_json, rack_env
          end
